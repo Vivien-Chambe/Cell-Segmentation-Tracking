be2794ba29e65c87a40f14493486543107a297c7
bouvieli <liloubouvier6@gmail.com>
2023-04-05 16:24:18 +0200

    cc

--- filtre.py
+++ filtre.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import sys
 
-path = r'Images3/puit03_t000.tif'
+path = r'images/puit03/t000.tif'
 img = cv.imread(path)
 if img is None:
     sys.exit("Could not read the image.")
