27d6b7d6d47b68512156d8b02b14dcfcf54ba799
Vivien Chambe <vivien.chambe@live.com>
2023-04-05 15:53:44 +0200

    classes.py

--- Classes.py
+++ Classes.py
@@ -1,1 +1,9 @@
-Classes
+## Class Cell 
+## Cette classe permettra de gerer et différencier nos cellules 
+class Cell:
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.state = state
+        self.neighbors = []
+        self.next_state = None
