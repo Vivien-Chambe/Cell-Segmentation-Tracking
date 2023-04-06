2b2f113930c67e086a6d42117c5879ecf37f63fe
Mai-san2901 <maissane.bouchema@hotmail.fr>
2023-04-06 18:14:59 +0200

    Semaine1: Debut de Labellisation (?)

--- Test.py
+++ Test.py
@@ -4,103 +4,123 @@ import scipy as sp
 from scipy.ndimage import maximum_filter
 from skimage import color
 
-# Normalisation de l'image + grayscale
+# Lecture de l'image + grayscale au cas où
 img = cv.imread("images/puit02/t000.tif")
 
 img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+# Affichage si besoin
 #cv.imshow('Avant normalisation:',img_gray)
 
+# Normalisation de l'image 
 img_norm = cv.normalize(img_gray, None, 0, 255, cv.NORM_MINMAX)
 
+# Affichage si besoin
 #cv.imshow('Apres normalisation:',img_norm)
 
-# Binarisation
-
+# Binarisation ret = valeur du threshold
 ret, img_bin = cv.threshold( img_norm, 122,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 
+# Affichage si besoin
 #cv.imshow('Apres binarisation:',img_bin)
 
-# enlever les bruits dès le debut....
-
+# On enleve les bruits dès le debut avec l'opening
 kernel = np.ones((5,5),np.uint8)
 img_open = cv.morphologyEx(img_bin,cv.MORPH_OPEN,kernel,iterations=1)
 
-# skimage....
+# On peut retirer les cellules qui se trouvent au bord de
+# l'image avec clear_border mais ce n'est pas ce qui est 
+# attendu (?)
 #img_open = sp.clear_border(img_open)
 
+# Affichage si besoin
 #cv.imshow('Apres opening (- bruits):',img_open)
 
-# on a des trous dans certaines cellules...
-# retirons les 
+# On a des trous dans certaines cellules
+# On les retire avec la fonction dilate 
+img_bg = cv.dilate(img_open,kernel,iterations = 1)
 
-# on utilise d'abord dilate
-img_bg = cv.dilate(img_open,np.ones((5,5),np.uint8),iterations = 1)
+# On appelle l'image obtenu, plus précisément les pixels noirs
+# le fond (background) on verra pourquoi dans la suite
 
+# Affichage si besoin
 #cv.imshow('Apres dilation:',img_bg)
 
-# "separation"
-
+# On veut ensuite
+# on utilise distanceTransform qui calcule la distance entre 
+# chaque pixel et le pixel non nul le plus proche
+# en gros c'est une maniere d'acceder au centre
 img_transform = cv.distanceTransform(img_open,cv.DIST_L2,5)
 
+# Affichage si besoin
 #cv.imshow('Apres dist transform:',img_transform)
  
+# On détermine ensuite le premier plan (foreground) cad nos cellules 
+# a l'aide de notre img_transform
+# Pour cela on appelle threshold encore une fois
+# "comparé au pixel d'intensité max au centre on prend que les pixel > 50% de cette intensité"
+# -> cela forme nos cellules 
 ret, img_fg = cv.threshold(img_transform,0.5*img_transform.max(),255,0)
+# on pourrait utiliser erode mais on perdrait trop d'information...
 
+# Affichage si besoin
 #cv.imshow('Apres separation:',img_fg)
 
-# on a notre foreground et notre background
-# on peut determiner les régions ambigues en calculant
-img_fg = np.uint8(img_fg)
+# On a notre foreground et notre background
+
+# On peut determiner les régions ambigues 
+# cad les zones où l'on est pas sûre d'etre sur une cellule
+# où sur le fond
+img_fg = np.uint8(img_fg) # on cast le foreground car contient des float
+# Les zones ambigues sont la soustractions dedu fond et du premier plan
 img_ambigu = cv.subtract(img_bg,img_fg) 
 
+#Affichage si besoin
 cv.imshow('regions ambigues:',img_ambigu)
 
-# labelisation 
-# on utilise connectecComponents qui label un groupe de pixel
-# en fct leur connectivité cad meme intensité et contigus
-
+# Labelisation 
+# On utilise connectecComponentswithStats qui associe a un groupe de pixel
+# une intensité de gris en fct leur connectivité (cad meme intensité et contigus)
+# Grace à elle on récupère les coordonnées du centre de ces composantes
+# soient nos cellules
 output= cv.connectedComponentsWithStats(img_fg)
 (nblabel, labels, stats, centroids) = output
-print(nblabel)
+
+# Pas sûre de la valeur de chaque val de retour
+# nblabels = nb de differents labels
+# Mais les centroids sont la dernière valeur de retour
+# et labels correspond aux couleurs associé aux composantes
+# (cellules)
+
+# Affichage si besoin
+#print(nblabel)
 #print(labels)
 #print(stats)
+# ptit print des centroids...
 print(centroids)
 
-#cv.imshow('markers:',markers)
+# Petit problème le background est d'intensité 0
+# Or on veut que seulement nos zônes ambigues soient d'intensite
+# 0 (pour la fonction watershed cf suite)
+# On augmente donc toutes les intensité de 10 par ex 
 labels = labels + 10
 
+# On associe nos region ambigue a l'intensité 0
 labels[img_ambigu==255] = 0
 
+# Enfin on appelle watershed
+# qui va nous permettre de segmenter nos cellules 
+# plus particulièrement celles qui se touchent
 labels = cv.watershed(img,labels)
 
+# watershed associe -1 aux bords...
+# On peut changer ca en lui associant du jaune parn exemple
 img[labels==-1]=[0,255,255]
 
+# On converti les labels de notre image en couleurs rgb 
 img_fin = color.label2rgb(labels,bg_label=0)
 
 cv.imshow('RESULTAT FINAL ',img_fin)
 
 cv.waitKey(0)
 
-# Labellisation / recherche des centroïdes
-## Il nous faut tout  d'abord les centroïdes de chaque cell
-## centroïdes = moyenne arithmetique de tous les points de la cell
-## = moyenne pondérée de tous les pixels constituant la cell.
-
-#cv.distanceTransform(img_norm, distanceType=2, maskSize=0)
-
-# On calcule les moments de l'image
-# moments d'une image = généralement des descripteurs de forme
-# -> on les utilise pour obtenir des caractéristiques globale d'une forme
-
-#moments = cv.moments(img_bin)
-
-#print(moments)
-
-# empirically set neighborhood_size to 20
-#neighborhood_size = 2
-#maximum_filter(img_norm, neighborhood_size) # ca marche passsssssssssssssssssssss
-
-#cv.imshow('Je suis l\'image avec les centroïdes',img_norm)
-
-#cv.waitKey(0)
