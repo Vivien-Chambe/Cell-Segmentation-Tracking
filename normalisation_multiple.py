b6a7d4471e77a81a4f8c060556c494c253655fae
Mai-san2901 <maissane.bouchema@hotmail.fr>
2023-04-07 10:32:18 +0200

    normalize multiple qui marche

--- normalisation_multiple.py
+++ normalisation_multiple.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import os
 
-def display_pic(folder):
+def normalize_pic(folder):
     paths=([x for x in os.walk(folder)])
 
     for i in paths:
@@ -10,12 +10,14 @@ def display_pic(folder):
         for filename in i[2]:
                 path=i[0]+"/"+filename
                 print(path)
-                a=cv.imread(path)
-                cv.imshow(path,a)
+                img=cv.imread(path)
+                cv.imshow(path,img)
+                img_normalized = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX)
+                cv.imwrite('puit02_clean/t00'+str(i)+'_clean.tif',img_normalized)
+                cv.imshow('puit02_clean/'+filename,img_normalized)
                 cv.waitKey(1000)
                 cv.destroyAllWindows()
 
 
- 
+normalize_pic("images")
 
-display_pic("images")
