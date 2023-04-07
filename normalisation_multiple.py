d7fa64f100cdbc05850acfdc54f56387692ff550
Mai-san2901 <maissane.bouchema@hotmail.fr>
2023-04-07 10:15:15 +0200

    Ajout de normalisation_multiple (?)

---
+++ normalisation_multiple.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 as cv
+import os
+
+def display_pic(folder):
+    paths=([x for x in os.walk(folder)])
+
+    for i in paths:
+        i[2].sort()
+        for filename in i[2]:
+                path=i[0]+"/"+filename
+                print(path)
+                a=cv.imread(path)
+                cv.imshow(path,a)
+                cv.waitKey(1000)
+                cv.destroyAllWindows()
+
+
+ 
+
+display_pic("images")
